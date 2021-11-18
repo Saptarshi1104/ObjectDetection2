8406f6c0aa2437ca5928e1388d802ab6c1593c07
Saptarshi Mandal <80155876+Saptarshi1104@users.noreply.github.com>
2021-11-18 16:02:52 +0530

    Add files via upload

---
+++ sofa.js
@@ -0,0 +1,31 @@
+img = "";
+status = "";
+
+function preload(){
+img = loadImage("pic4.jpg");
+}
+
+function setup(){
+canvas = createCanvas(640, 420);
+canvas.center();
+
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw(){
+image(img, 0, 0, 640, 420);
+}
+
+function modelLoaded(){
+console.log("Model Loaded!");
+status = true;
+objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, result){
+if(error){
+console.log(error);
+}
+console.log(results);
+}
